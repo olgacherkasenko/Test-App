16bbdf6db028a9e4d3787a8a2f47f6ab33df7b23
Olga Cherkasenko <cherkasolga1997@gmail.com>
2020-01-06 18:19:01 +0200

    changed class component to functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import axios from 'axios';
 import Aux from './hoc/Auxiliary';
 import sortUsers from './utils/sortUsers';
@@ -8,52 +8,98 @@ import MonthsList from './components/MonthsList/MonthsList';
 import UsersList from './components/UsersList/UsersList';
 import './App.css';
 
-class App extends Component {
-  state = {
-    people: [],
-    activeId: 0,
-    isHovered: false
+// class App extends Component {
+//   state = {
+//     people: [],
+//     activeId: 0,
+//     isHovered: false
+//   }
+
+//   componentDidMount() {
+//     axios.get('https://yalantis-react-school.herokuapp.com/api/task0/users')
+//         .then(res => {
+//             let people = sortUsers(res.data);
+//             this.setState({people: people});
+//         })
+//   } 
+
+//   onMouseLeaveHandler = () => {
+//     this.setState({isHovered: false})
+//   }
+
+//   onMouseEnterHandler = ({target}) => {
+//     const currentId = parseInt(target.id);
+//     this.setState({activeId: currentId, isHovered: true})
+//   }
+
+//   render () {
+//     const isLoading = this.state.people.length === 0;
+//     let bgColors = [];
+
+//     if (!isLoading) {
+//       this.state.people.forEach(user => {
+//           let bgColor = chooseBgColor(user.amount);
+//           bgColors.push(bgColor);
+//       });
+//     }
+
+//     return (
+//       <div className="App">
+//           {isLoading ? <Spinner /> :
+//             <Aux>
+//                 <MonthsList users={this.state.people} backgrounds={bgColors} mouseEnter={this.onMouseEnterHandler} mouseLeave={this.onMouseLeaveHandler}/>
+//                 {this.state.isHovered ? <UsersList users={this.state.people} id={this.state.activeId}/> : ''}
+//             </Aux> 
+//           }
+//       </div>
+//     )
+//   }
+// }
+
+function App() {
+  const [people, setPeople] = useState([]);
+  const [activeId, setActiveId] = useState(0);
+  const [isHovered, setIsHovered] = useState(false);
+  const isLoading = people.length === 0;
+
+  let bgColors = [];
+  if (!isLoading) {
+    people.forEach(user => {
+        let bgColor = chooseBgColor(user.amount);
+        bgColors.push(bgColor);
+    });
   }
 
-  componentDidMount() {
+  useEffect(() => {
     axios.get('https://yalantis-react-school.herokuapp.com/api/task0/users')
-        .then(res => {
-            let people = sortUsers(res.data);
-            this.setState({people: people});
-        })
-  } 
+      .then(res => {
+          let people = sortUsers(res.data);
+          setPeople(people);
+      })
+  }, [])
 
-  onMouseLeaveHandler = () => {
-    this.setState({isHovered: false})
+  const onMouseLeaveHandler = () => {
+    setIsHovered(false);
   }
 
-  onMouseEnterHandler = ({target}) => {
+  const onMouseEnterHandler = ({target}) => {
     const currentId = parseInt(target.id);
-    this.setState({activeId: currentId, isHovered: true})
+    setIsHovered(true);
+    setActiveId(currentId);
   }
-
-  render () {
-    const isLoading = this.state.people.length === 0;
-    let bgColors = [];
-
-    if (!isLoading) {
-      this.state.people.forEach(user => {
-          let bgColor = chooseBgColor(user.amount);
-          bgColors.push(bgColor);
-      });
-    }
 
     return (
       <div className="App">
           {isLoading ? <Spinner /> :
             <Aux>
-                <MonthsList users={this.state.people} backgrounds={bgColors} mouseEnter={this.onMouseEnterHandler} mouseLeave={this.onMouseLeaveHandler}/>
-                {this.state.isHovered ? <UsersList users={this.state.people} id={this.state.activeId}/> : ''}
+                <MonthsList users={people} backgrounds={bgColors} mouseEnter={onMouseEnterHandler} mouseLeave={onMouseLeaveHandler}/>
+                {isHovered ? <UsersList users={people} id={activeId}/> : ''}
             </Aux> 
           }
       </div>
     )
-  }
 }
+
+  
 
 export default App;
